b286ca68aa588f779b789474f0361ef4ae3c20ac
Koczan <62504952+Koczan@users.noreply.github.com>
2023-11-10 17:59:17 +0100

    Update streamlit_app.py
    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,10 @@ my_data_row = my_cur.fetchone()
 streamlit.text("The fruit load list contains:")
 streamlit.text(my_data_row)
 
+fruit_choice = streamlit.text_input("What fruit would you like to add?", "jackfruit");
+streamlit.text("Thanks for adding" + fruit_choice);
+
+
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 #fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 #streamlit.write('The user entered ', fruit_choice)
