81e47dbe9f6fdf0bd913d5d78acec61ba0decbc3
Koczan <62504952+Koczan@users.noreply.github.com>
2023-11-10 02:06:56 +0100

    Update streamlit_app.py
    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,11 +24,20 @@ streamlit.dataframe(fruits_to_show)
 
 # new section to display fruityvice api details
 streamlit.header("Fruityvice Fruit Advice!")
-import requests
-# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
 streamlit.write('The user entered ', fruit_choice)
+
+import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+
+
+
+
+
+# fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+#streamlit.write('The user entered ', fruit_choice)
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 
 # streamlit.text(fruityvice_response.json())
 
@@ -36,4 +45,4 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_c
 # fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 # show table using pandas
-streamlit.dataframe(fruityvice_normalized)
+#streamlit.dataframe(fruityvice_normalized)
