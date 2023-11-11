f7ffc3c8385c79b8bc29a413121edd58e480e0ff
Koczan <62504952+Koczan@users.noreply.github.com>
2023-11-11 17:33:19 +0100

    Update streamlit_app.py
    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,8 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 # new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-# dont run anything past here while we troubleshoot
-#streamlit.stop();
+
 try:
   fruit_choice = streamlit.text_input("What fruit would you like information about?","Kiwi")
   if not fruit_choice:
@@ -42,18 +41,20 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
+
 #import requests
 
+# dont run anything past here while we troubleshoot
+streamlit.stop();
 
 
 
-
-#my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"]);
-#my_cur = my_cnx.cursor();
-#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list");
-#my_data_row = my_cur.fetchone();
-#streamlit.text("The fruit load list contains:");
-#streamlit.text(my_data_row);
+my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"]);
+my_cur = my_cnx.cursor();
+my_cur.execute("select * from pc_rivery_db.public.fruit_load_list");
+my_data_row = my_cur.fetchone();
+streamlit.text("The fruit load list contains:");
+streamlit.text(my_data_row);
 
 #fruit_choice = streamlit.text_input("What fruit would you like to add?", "jackfruit");
 #streamlit.text("Thanks for adding " + fruit_choice);
