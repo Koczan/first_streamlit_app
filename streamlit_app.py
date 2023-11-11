a25e35d0dc2dcf91ee4e4feaec3ab42aa50dfec8
Koczan <62504952+Koczan@users.noreply.github.com>
2023-11-11 17:26:32 +0100

    Update streamlit_app.py
    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -48,18 +48,18 @@ try:
 
 
 
-my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"]);
-my_cur = my_cnx.cursor();
-my_cur.execute("select * from pc_rivery_db.public.fruit_load_list");
-my_data_row = my_cur.fetchone();
-streamlit.text("The fruit load list contains:");
-streamlit.text(my_data_row);
+#my_cnx = snowflake.connector.connect(streamlit.secrets["snowflake"]);
+#my_cur = my_cnx.cursor();
+#my_cur.execute("select * from pc_rivery_db.public.fruit_load_list");
+#my_data_row = my_cur.fetchone();
+#streamlit.text("The fruit load list contains:");
+#streamlit.text(my_data_row);
 
-fruit_choice = streamlit.text_input("What fruit would you like to add?", "jackfruit");
-streamlit.text("Thanks for adding " + fruit_choice);
+#fruit_choice = streamlit.text_input("What fruit would you like to add?", "jackfruit");
+#streamlit.text("Thanks for adding " + fruit_choice);
 
 #this will not work correctly, but just go with it for now
-my_cur.execute("insert into fruit_load_list values ('from streamlit')");
+#my_cur.execute("insert into fruit_load_list values ('from streamlit')");
 
 
 # fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
