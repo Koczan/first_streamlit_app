f2c496dfa5b52ba00761a9ee9e6669b7e35ec8d4
Koczan <62504952+Koczan@users.noreply.github.com>
2023-11-11 17:39:07 +0100

    Update streamlit_app.py
    x

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,7 +34,7 @@ def get_fruityvice_data(this_fruit_choice):
 
 # new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-"""
+
 try:
   fruit_choice = streamlit.text_input("What fruit would you like information about?","Kiwi")
   if not fruit_choice:
@@ -42,7 +42,7 @@ try:
   else:
     back_from_function = get_fruityvice_data(fruit_choice)
     streamlit.dataframe(back_from_function)
-
+"""
 #import requests
 
 # dont run anything past here while we troubleshoot
